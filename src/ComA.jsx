5e12d3966e5c5207c6b2c8c141fe13743c06c0b8
mdashik7723 <mdashik7723@gmail.com>
2022-09-12 01:09:40 +0600

    --

--- src/ComA.jsx
+++ src/ComA.jsx
@@ -3,15 +3,15 @@ import axios from "axios";
 
 const ComA = () => {
 
-    const [num, setNum] = useState();
+    const [number, setNumber] = useState();
     const [name, setName] = useState();
     const [ moves,setMoves] = useState();
 
     useEffect(() => {
 
         async function getData() {
-            const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`);
-            console.log(res.data);
+            const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${number}`);
+            // console.log(res.data);
             setName(res.data.name);
             setMoves(res.data.moves.length);
         }
@@ -21,14 +21,14 @@ const ComA = () => {
 
     return (
         <>
-            <h1> You Choose <span style={{color: 'red'}}> {num} value </span></h1>
+            <h1> You Choose <span style={{color: 'red'}}> {number} value </span></h1>
             <h1> My name is <span style={{color: 'red'}}> {name} </span></h1>
             <h1> I have <span style={{color: 'red'}}> {moves} moves </span></h1>
 
             <select
-                value={num}
+                value={number}
                 onChange={(event) => {
-                    setNum(event.target.value);
+                    setNumber(event.target.value);
                 }}>
                 <option value='1'> 1</option>
                 <option value='25'> 25</option>
